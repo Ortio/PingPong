a665d9e8d869966258d638319b09c82698d6eb0c
Murad Ilyasov <murad.ilyasov@sibmbirsoft.com>
2022-05-05 16:38:03 +0300

    Init project

---
+++ src/Controller/PingPong.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class PingPong
+{
+    public function index(): void
+    {
+
+    }
+
+}
